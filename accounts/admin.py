3dbc32ca436388192c35c2dfb5c7335cb4e20731
ikbolpm <36568151+ikbolpm@users.noreply.github.com>
2018-09-22 15:04:05 +0500

    Update admin.py
    Modified

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model, admin as user_admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext as _
 
 User = get_user_model()
-
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ['email']
-    list_display = ['email', 'is_staff', 'is_admin', 'last_login', 'created']
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('full_name',  )}),
-        (_('Permissions'), {'fields': ('is_active', 'staff', 'admin', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', )}),
-    )
-    class Meta:
-        model = User
 
 admin.site.register(User, UserAdmin)
