3d8ead3b279403a8c53591929731d0e4b353c590
ikbolpm <36568151+ikbolpm@users.noreply.github.com>
2018-09-22 14:52:27 +0500

    Update admin.py
    Modification of Admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,16 @@ from django.contrib.auth import get_user_model, admin as user_admin
 
 User = get_user_model()
 
-class UserAdmin(user_admin.UserAdmin):
+class UserAdmin(admin.ModelAdmin):
     search_fields = ['email']
     list_display = ['email', 'is_staff', 'is_admin', 'last_login', 'created']
-
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('full_name',  )}),
+        (_('Permissions'), {'fields': ('is_active', 'staff', 'admin', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'created')}),
+    )
     class Meta:
         model = User
 
