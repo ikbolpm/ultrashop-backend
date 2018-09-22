5d810986cf419b2a7be565fb436799864580b831
ikbolpm <36568151+ikbolpm@users.noreply.github.com>
2018-09-22 14:38:09 +0500

    Update admin.py
    Fixed user admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, admin as user_admin
 
 User = get_user_model()
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(user_admin.UserAdmin):
     search_fields = ['email']
     list_display = ['email', 'is_staff', 'is_admin', 'last_login', 'created']
 
