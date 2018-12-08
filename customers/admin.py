56a2a8fdfbaee0a4d891c51f70fdd9aecd17f15e
ikbolpm <ikbolpm@gmail.com>
2018-12-08 13:52:47 +0500

    Removed laptop from Customer model

--- customers/admin.py
+++ customers/admin.py
@@ -5,7 +5,7 @@ from .models import Customer
 
 
 class CustomerAdmin(ModelAdmin):
-    list_display = ['name', 'laptop', 'phone', 'email', 'created', 'updated']
+    list_display = ['name', 'phone', 'email', 'created', 'updated']
     list_display_links = ['name', ]
     list_editable = ['phone', 'email']
     search_fields = ['name', 'phone', 'email']
--- customers/models.py
+++ customers/models.py
@@ -7,8 +7,6 @@ class Customer(models.Model):
     name = models.CharField(max_length=255, help_text='Полное имя покупателя. Например: Кобулов Икбол')
     phone = models.CharField(max_length=15, help_text='Номер телефона покупателя в формате 998 91 192 2228')
     email = models.EmailField(blank=True, null=True)
-    laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, help_text='Какой ноутбук купили? Выберите из списка',
-                               null=True, blank=True)
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
