cef2a388150b9048808f30316489a7c3a9d5e41c
ikbolpm <ikbolpm@gmail.com>
2018-12-16 18:43:48 +0500

    Made profit editable in admin sales

--- sales/admin.py
+++ sales/admin.py
@@ -7,7 +7,7 @@ from .models import Sales
 class SalesAdmin(ModelAdmin):
     list_display = ['laptop', 'customer', 'sold_by', 'warehouse', 'price', 'profit', 'quantity', 'created']
     list_display_links = ['laptop', ]
-    # list_editable = ['customer', 'warehouse', 'price', 'quantity', ]
+    list_editable = ['profit']
     search_fields = ['customer__name', 'laptop__name', 'price']
     list_filter = ['warehouse', 'sold_by']
     autocomplete_fields = ['laptop']
