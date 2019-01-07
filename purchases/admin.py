c761350c1d1d66aa1b298f775fbf201233028389
ikbolpm <ikbolpm@gmail.com>
2019-01-07 23:24:14 +0500

    Changed filters in Sales, Purchases and Inventory

--- purchases/admin.py
+++ purchases/admin.py
@@ -8,7 +8,14 @@ class PurchaseAdmin(ModelAdmin):
     list_display = ['laptop', 'warehouse', 'cost', 'quantity', 'created', 'updated']
     list_display_links = ['laptop', ]
     list_editable = ['warehouse', 'cost', 'quantity']
-    list_filter = ['warehouse', 'laptop__brand', 'laptop__screen_size', 'laptop__graphics_card', 'laptop__processor']
+    list_filter = (
+        ('laptop__brand', admin.RelatedOnlyFieldListFilter),
+        ('laptop__screen_size', admin.RelatedOnlyFieldListFilter),
+        ('laptop__graphics_card', admin.RelatedOnlyFieldListFilter),
+        ('laptop__processor', admin.RelatedOnlyFieldListFilter),
+        ('warehouse', admin.RelatedOnlyFieldListFilter),
+        ('created'),
+    )
     autocomplete_fields = ['laptop']
 
 admin.site.register(Purchase, PurchaseAdmin)
