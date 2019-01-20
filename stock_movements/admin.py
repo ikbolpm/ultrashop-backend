751673974428ef6d5d2eaa9a0f3eb6623c752fd1
Ikbol <ikbolpm@gmail.com>
2019-01-20 22:16:11 +0500

    Additional filters to Stock MOvements

--- stock_movements/admin.py
+++ stock_movements/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from sales.admin import ExportCsvMixin
 
 from .models import StockMovements
 
 
-class StockMovementsAdmin(ModelAdmin):
+class StockMovementsAdmin(ModelAdmin, ExportCsvMixin):
     list_display = ['laptop', 'from_warehouse', 'to_warehouse', 'quantity', 'moved_by', 'created', 'updated']
     list_display_links = ['laptop', ]
     # search_fields = ['laptop__name', 'laptop__model']
-    list_editable = ['from_warehouse', 'to_warehouse', 'quantity', 'moved_by']
+    #list_editable = ['from_warehouse', 'to_warehouse', 'quantity', 'moved_by']
     exclude = ['moved_by']
     list_filter = (
+        'created',
         ('moved_by', admin.RelatedOnlyFieldListFilter),
         ('from_warehouse', admin.RelatedOnlyFieldListFilter),
         ('to_warehouse', admin.RelatedOnlyFieldListFilter),
+        ('laptop__brand', admin.RelatedOnlyFieldListFilter),
+        ('laptop__laptop_type', admin.RelatedOnlyFieldListFilter),
+        ('laptop__screen_size', admin.RelatedOnlyFieldListFilter),
+        ('laptop__graphics_card', admin.RelatedOnlyFieldListFilter),
+        ('laptop__processor', admin.RelatedOnlyFieldListFilter),
     )
     autocomplete_fields = ['laptop']
 
