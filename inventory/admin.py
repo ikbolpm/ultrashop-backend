8bcca0c32d33dc5cdf5df1c76c1cc0a2a2999589
Ikbol Kobulov <ikbolpm@gmail.com>
2019-04-05 00:20:12 +0500

    Rounded up the price to thousands.

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from sales.admin import ExportCsvMixin
 from settings.models import DollarExchangeRate, TransactionCoefficient
+import math
 
 
 from .models import Inventory
@@ -26,7 +27,7 @@ class InventoryAdmin(ModelAdmin, ExportCsvMixin):
 
     def laptop__price(self, obj):
         uzs_price = (obj.laptop.price * DollarExchangeRate.objects.filter().first().exchange_rate / TransactionCoefficient.objects.filter().first().coefficient)
-        uzs_price = round(uzs_price, 0)
+        uzs_price = int(math.ceil(uzs_price / 1000)) * 1000
         uzs_price = '{:,.0f}'.format(uzs_price) + ' сум'
         return uzs_price
 
