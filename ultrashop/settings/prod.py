4fd11047af98a644a8c039e238f47e1e3e37ea01
ikbol <ikbolpm@gmail.com>
2019-09-25 13:34:38 +0500

    Added Product Model

--- ultrashop/settings/prod.py
+++ ultrashop/settings/prod.py
@@ -14,8 +14,7 @@ DATABASES = {
 
 WSGI_APPLICATION = 'ultrashop.wsgi_prod.application'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__), '..', '..', 'static')
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT='static'
 
 CORS_ORIGIN_WHITELIST = (
 	'ultrashop.uz',
