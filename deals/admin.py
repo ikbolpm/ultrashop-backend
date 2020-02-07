40013345fe7fcc2b5809d3f07619987bcfaa2eac
ikbolpm <ikbolpm@gmail.com>
2020-02-07 07:57:13 +0500

    Added shop, orders

--- deals/admin.py
+++ deals/admin.py
@@ -7,5 +7,10 @@ class DealAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_editable = ['active']
     save_as = True
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
 
 admin.site.register(Deal, DealAdmin)
