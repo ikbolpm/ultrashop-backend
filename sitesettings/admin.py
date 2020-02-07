10ddcafb87188d4f6deac5d71e027144e79277ad
ikbolpm <ikbolpm@gmail.com>
2020-02-07 07:55:45 +0500

    Added shop, orders

--- sitesettings/admin.py
+++ sitesettings/admin.py
@@ -4,11 +4,21 @@ from .models import DollarExchangeRate, TransactionCoefficient
 
 class DollarExchangeRateAdmin(admin.ModelAdmin):
     list_display = ['exchange_rate', 'created', 'updated']
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
 
 admin.site.register(DollarExchangeRate, DollarExchangeRateAdmin)
 
 
 class TransactionCoefficientAdmin(admin.ModelAdmin):
     list_display = ['coefficient', 'created', 'updated']
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
 
 admin.site.register(TransactionCoefficient, TransactionCoefficientAdmin)
