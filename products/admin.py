fd69efc488b1f87592d237151804ed7a061d7f9d
Ikbol Kobulov <ikbolpm@gmail.com>
2020-09-24 15:14:55 +0500

    Unhide inquiries in admin

--- products/admin.py
+++ products/admin.py
@@ -39,7 +39,7 @@ class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
 
         meta = self.model._meta
-        field_names = ['brand', 'category', 'name', 'model', 'old_price', 'price']
+        field_names = ['brand__name', 'category__name', 'name', 'model', 'old_price', 'price']
 
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
