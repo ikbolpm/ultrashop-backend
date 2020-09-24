1769111dff8f2c230b38f35cf244a59268807d69
Ikbol Kobulov <ikbolpm@gmail.com>
2020-09-24 15:13:31 +0500

    Unhide inquiries in admin

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,6 @@ from django.contrib.admin import ModelAdmin
 from django.shortcuts import get_object_or_404
 from custom_multiupload.admin import MultiUploadAdmin
 from django.conf import settings
-from sales.admin import ExportCsvMixin
 
 
 
@@ -34,6 +33,25 @@ class GalleryMultiuploadMixing(object):
             'id': image.id,
             'name': image.filename
         }
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = ['brand', 'category', 'name', 'model', 'old_price', 'price']
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 class ProductAdmin(GalleryMultiuploadMixing, MultiUploadAdmin, ExportCsvMixin):
